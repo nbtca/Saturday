2b66481fe114f21f56f77ec018427fe79281c8e3
Jolanyu <1258032547@qq.com>
2021-10-09 20:12:55 +0800

    fix bug: no such user

--- controller/login.js
+++ controller/login.js
@@ -8,46 +8,48 @@ class Login {
     let rid = req.body.id;
     let password = req.body.password;
     try {
-      let dbResults = await ElementModel.findByFilter(["ralias", "ravatar", "role", "rpassword"], { rid: rid });
-      console.log(dbResults[0]);
-      if (dbResults == null) {
+      let dbResults = await ElementModel.findByFilter(
+        ["ralias", "ravatar", "role", "rpassword"],
+        { rid: rid }
+      );
+      if (dbResults.length == 0) {
         respond(res, 1010, "No such user");
       } else {
-        if (1) {
-          console.log(dbResults[0].rpassword);
-          if (password == dbResults[0].rpassword || (password == "" && dbResults[0].rpassword == null)) {
-            let isActivated = password != "" ? true : false;
-            let role = dbResults[0].role == 2 ? "admin" : "element";
-            let info = {
-              rid: rid,
-              role: role,
-            };
-            let token = jwt.sign(
-              {
-                exp: Math.floor(Date.now() / 1000) + 24 * 60 * 6000,
-                data: info,
-              },
-              cert
-            );
-            let data = {
-              token: token,
-              alias: dbResults[0].ralias,
-              avatar: dbResults[0].ravatar,
-              rid: rid,
-              role: role,
-              isActivated: isActivated,
-            };
-            await ElementModel.update(
-              {
-                gmt_modified: new Date(),
-              },
-              { rid: rid }
-            );
-            // TODO auto
-            respond(res, 0, "Success", data);
-          } else {
-            respond(res, 1011, "Wrong password");
-          }
+        if (
+          password == dbResults[0].rpassword ||
+          (password == "" && dbResults[0].rpassword == null)
+        ) {
+          let isActivated = password != "" ? true : false;
+          let role = dbResults[0].role == 2 ? "admin" : "element";
+          let info = {
+            rid: rid,
+            role: role,
+          };
+          let token = jwt.sign(
+            {
+              exp: Math.floor(Date.now() / 1000) + 24 * 60 * 6000,
+              data: info,
+            },
+            cert
+          );
+          let data = {
+            token: token,
+            alias: dbResults[0].ralias,
+            avatar: dbResults[0].ravatar,
+            rid: rid,
+            role: role,
+            isActivated: isActivated,
+          };
+          await ElementModel.update(
+            {
+              gmt_modified: new Date(),
+            },
+            { rid: rid }
+          );
+          // TODO auto
+          respond(res, 0, "Success", data);
+        } else {
+          respond(res, 1011, "Wrong password");
         }
       }
     } catch (err) {
