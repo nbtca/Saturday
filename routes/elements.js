e8f950fba4421e2f0f67841fb554e85fa75965b2
templari <q79142466@163.com>
2021-10-10 09:35:38 +0800

    remove 'test'

--- routes/elements.js
+++ routes/elements.js
@@ -1,11 +1,9 @@
 var express = require("express");
 const router = express.Router();
 const formidableMiddleware = require("express-formidable");
-const { test } = require("../test");
 var Element = require("../controller/element/element");
 
 router.get("/", Element.getAll);
-// router.get("/test", (e) => test(e));
 router.get("/:rid", Element.get);
 router.post("/", Element.create);
 router.put("/", Element.update);
