e3b56f510eb73584f97ef59592ad763a81d95321
Jolanyu <1258032547@qq.com>
2021-10-19 15:54:01 +0800

    save

--- controller/element/element.js
+++ controller/element/element.js
@@ -1,77 +1,154 @@
-const { respond, dateToStr, put } = require("../../utils/utils");
+const log4js = require("../../utils/log4js");
+const { respond, dateToStr, put, createToken } = require("../../utils/utils");
 const ElementModel = require("../../models/ElementModel");
 class ElementController {
   constructor() {
-    // ElementModel = new ElementModel();
   }
   getAll(req, res, next) {
-    try {
-      ElementModel.findByFilterOrder({ exclude: ["rpassword"] }, {}, [["gmt_modified", "DESC"]]).then(result => {
+    ElementModel.findByFilterOrder({ exclude: ["rpassword"] }, {}, [
+      ["gmt_modified", "DESC"],
+    ])
+      .then(result => {
         for (let item of result) {
           //TODO gmt_create format
           item.dataValues.gmt_create = dateToStr(item.gmt_create, "time");
           item.dataValues.gmt_modified = dateToStr(item.gmt_modified, "time");
         }
         respond(res, 0, "Success", result);
-      });
-    } catch (error) {
-      console.error(error);
-    }
+      })
+      .catch(error => console.log(error));
   }
   get(req, res, next) {
-    try {
-      ElementModel.findByFilter({ exclude: ["rpassword"] }, { rid: req.params.rid }).then(result => {
-        result ? respond(res, 0, "Success", result) : respond(res, 123, "no such element");
-      });
-    } catch (error) {
-      next(error);
-    }
+    ElementModel.findByFilter(
+      { exclude: ["rpassword"] },
+      { rid: req.params.rid }
+    )
+      .then(result => {
+        result
+          ? respond(res, 0, "Success", result)
+          : respond(res, 123, "no such element");
+      })
+      .catch(error => console.log(error));
   }
-  async create(req, res, next) {
-    try {
-      await ElementModel.create({
-        rid: req.body.rid,
-        alias: req.body.alias,
-        password: req.body.password,
+  create(req, res, next) {
+    ElementModel.create({
+      rid: req.body.rid,
+      alias: req.body.alias,
+      password: req.body.password,
+      name: req.body.name,
+      class: req.body.class,
+      rqq: req.body.rqq,
+      rphone: req.body.rphone,
+      ravatar: req.body.ravatar,
+      profile: req.body.profile,
+      created_by: res.locals.data.rid,
+      gmt_create: new Date(),
+      gmt_modified: new Date(),
+      // TODO:gmt_expire
+    })
+      .then(res => respond(res, 0))
+      .catch(error => console.log(error));
+  }
+  update(req, res, next) {
+    ElementModel.update(
+      {
+        rid: res.locals.data.rid,
+        ralias: req.body.alias,
+        rpassword: req.body.password,
         name: req.body.name,
         class: req.body.class,
         rqq: req.body.rqq,
         rphone: req.body.rphone,
-        ravatar: req.body.ravatar,
-        profile: req.body.profile,
-        created_by: res.locals.data.rid,
-        gmt_create: new Date(),
-        gmt_modified: new Date(),
-        // TODO:gmt_expire
+      },
+      { rid: res.locals.data.rid }
+    )
+      .then(respond(res, 0))
+      .catch(error => console.log(error));
+  }
+  delete(req, res, next) {
+    ElementModel.delete({
+      rid: req.body.rid,
+    })
+      .then(result => {
+        // console.log(result);
+        respond(res, 0);
+      })
+      .catch(error => console.log(error));
+  }
+  login(req, res, next) {
+    let rid = req.body.id;
+    let password = req.body.password;
+    ElementModel.findByFilter(
+      ["ralias", "rpassword", "ravatar", "role", "status"],
+      { rid: rid }
+    )
+      .then(async dbResults => {
+        if (dbResults.length == 0) {
+          respond(res, 1010, "No such user");
+        } else {
+          let elementInfo = dbResults[0];
+          let roleMap = ["", "element", "admin"];
+          if (
+            password == elementInfo.rpassword ||
+            (password == "" && elementInfo.rpassword == null)
+          ) {
+            let role =
+              elementInfo.status == 0
+                ? "notActivated"
+                : roleMap[elementInfo.role];
+            let token = createToken(100, {
+              rid: rid,
+              role: role,
+            });
+            let data = {
+              token: token,
+              alias: elementInfo.ralias,
+              avatar: elementInfo.ravatar,
+              rid: rid,
+              role: role,
+            };
+            await ElementModel.update(
+              { gmt_modified: new Date() },
+              { rid: rid }
+            );
+
+            let logger = log4js.getLogger();
+            logger.info(rid);
+
+            respond(res, 0, "Success", data);
+          } else {
+            respond(res, 1011, "Wrong password");
+          }
+        }
+      })
+      .catch(error => {
+        console.log(error);
       });
-      respond(res, 0);
-    } catch (error) {
-      next(error);
-    }
   }
   activate(req, res, next) {
     // try {
-      // let file = req.files.file;
-      // let ext = "." + file.type.substring(file.type.indexOf("/") + 1);
-      // let timestamps = new Date().getTime();
-      // let fileName = "/element/" + res.locals.data.rid + "/" + timestamps + ext;
-      // let path = req.files.file.path;
-      // console.log(fileName, path);
-      // put(fileName, path).then(result => {
-      console.log(1);
-      ElementModel.update(
-        {
-          // rid: res.locals.data.rid,
-          ralias: req.body.alias,
-          rpassword: req.body.password,
-          // name: req.body.name,
-          // class: req.body.class,
-          rqq: req.body.rqq,
-          rphone: req.body.rphone,
-        },
-        { rid: res.locals.data.rid }
-      ).then(respond(res, 0));
-      // });
+    // let file = req.files.file;
+    // let ext = "." + file.type.substring(file.type.indexOf("/") + 1);
+    // let timestamps = new Date().getTime();
+    // let fileName = "/element/" + res.locals.data.rid + "/" + timestamps + ext;
+    // let path = req.files.file.path;
+    // console.log(fileName, path);
+    // put(fileName, path).then(result => {
+    ElementModel.update(
+      {
+        // rid: res.locals.data.rid,
+        ralias: req.body.alias,
+        rpassword: req.body.password,
+        // name: req.body.name,
+        // class: req.body.class,
+        rqq: req.body.rqq,
+        rphone: req.body.rphone,
+      },
+      { rid: res.locals.data.rid }
+    )
+      .then(respond(res, 0))
+      .catch(error => console.log(error));
+    // });
     // } catch (err) {
     //   console.log(err);
     // }
@@ -104,16 +181,15 @@ class ElementController {
   //     console.log(err);
   //   }
   // }
-
   updateAvatar(req, res, next) {
-    try {
-      let file = req.files.file;
-      let ext = "." + file.type.substring(file.type.indexOf("/") + 1);
-      let timestamps = new Date().getTime();
-      let fileName = "/element/" + res.locals.data.rid + "/" + timestamps + ext;
-      let path = req.files.file.path;
-      console.log(fileName, path);
-      put(fileName, path).then(result => {
+    let file = req.files.file;
+    let ext = "." + file.type.substring(file.type.indexOf("/") + 1);
+    let timestamps = new Date().getTime();
+    let fileName = "/element/" + res.locals.data.rid + "/" + timestamps + ext;
+    let path = req.files.file.path;
+    console.log(fileName, path);
+    put(fileName, path)
+      .then(result => {
         console.log(result);
         ElementModel.update(
           {
@@ -121,41 +197,8 @@ class ElementController {
           },
           { rid: res.locals.data.rid }
         ).then(respond(res, 0));
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  update(req, res, next) {
-    try {
-      ElementModel.update(
-        {
-          rid: res.locals.data.rid,
-          ralias: req.body.alias,
-          rpassword: req.body.password,
-          name: req.body.name,
-          class: req.body.class,
-          rqq: req.body.rqq,
-          rphone: req.body.rphone,
-        },
-        { rid: res.locals.data.rid }
-      ).then(respond(res, 0));
-    } catch (err) {
-      next(err);
-    }
-  }
-  delete(req, res, next) {
-    try {
-      ElementModel.delete({
-        rid: req.body.rid,
-      }).then(result => {
-        // console.log(result);
-        respond(res, 0);
-      });
-    } catch (err) {
-      next(err);
-    }
+      })
+      .catch(error => console.log(error));
   }
 }
 module.exports = new ElementController();
--- controller/login.js
+++ controller/login.js
@@ -1,58 +1,56 @@
-const jwt = require("jsonwebtoken");
 const log4js = require("../utils/log4js");
-const { respond } = require("../utils/utils");
-const { cert } = require("../config/config");
+const { respond, createToken } = require("../utils/utils");
 const ElementModel = require("../models/ElementModel");
 class Login {
   constructor() {}
   async login(req, res, next) {
     let rid = req.body.id;
     let password = req.body.password;
-    try {
-      let dbResults = await ElementModel.findByFilter(["ralias", "rpassword", "ravatar", "role", "status"], { rid: rid });
-      if (dbResults.length == 0) {
-        respond(res, 1010, "No such user");
-      } else {
-        let elementInfo = dbResults[0];
-        let roleMap = ["", "element", "admin"];
-        if (password == elementInfo.rpassword || (password == "" && elementInfo.rpassword == null)) {
-          let role = roleMap[elementInfo.role];
-          if (elementInfo.status == 0) role = "notActivated";
-          let info = {
-            rid: rid,
-            role: role,
-          };
-          let token = jwt.sign(
-            {
-              exp: Math.floor(Date.now() / 1000) + 24 * 60 * 6000,
-              data: info,
-            },
-            cert
-          );
-          let data = {
-            token: token,
-            alias: elementInfo.ralias,
-            avatar: elementInfo.ravatar,
-            rid: rid,
-            role: role,
-          };
-          await ElementModel.update(
-            {
-              gmt_modified: new Date(),
-            },
-            { rid: rid }
-          );
-          // TODO auto
-          let logger = log4js.getLogger();
-          logger.info(rid);
-          respond(res, 0, "Success", data);
+     ElementModel.findByFilter(
+      ["ralias", "rpassword", "ravatar", "role", "status"],
+      { rid: rid }
+    )
+      .then(async dbResults => {
+        if (dbResults.length == 0) {
+          respond(res, 1010, "No such user");
         } else {
-          respond(res, 1011, "Wrong password");
+          let elementInfo = dbResults[0];
+          let roleMap = ["", "element", "admin"];
+          if (
+            password == elementInfo.rpassword ||
+            (password == "" && elementInfo.rpassword == null)
+          ) {
+            let role =
+              elementInfo.status == 0
+                ? "notActivated"
+                : roleMap[elementInfo.role];
+            let token = createToken(100, {
+              rid: rid,
+              role: role,
+            });
+            let data = {
+              token: token,
+              alias: elementInfo.ralias,
+              avatar: elementInfo.ravatar,
+              rid: rid,
+              role: role,
+            };
+            await ElementModel.update(
+              { gmt_modified: new Date() },
+              { rid: rid }
+            );
+
+            let logger = log4js.getLogger();
+            logger.info(rid);
+            respond(res, 0, "Success", data);
+          } else {
+            respond(res, 1011, "Wrong password");
+          }
         }
-      }
-    } catch (err) {
-      next(err);
-    }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
 module.exports = new Login();
