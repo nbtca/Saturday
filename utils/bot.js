fd52b1cfadaa2e1e1a2e639cbb1e25dc17796e4d
templari <q79142466@163.com>
2022-01-13 23:41:29 +0800

    add null judgment for model in bot message

--- utils/bot.js
+++ utils/bot.js
@@ -9,15 +9,22 @@ class Bot {
   newEventTemplate(event) {
     // let xmlContent =
     //   "<?xml version='1.0' encoding='UTF-8' standalone='yes' ?> <msg serviceID='1' templateID='-1' action='app' actionData='com.android.camera' brief='测试' sourceMsgId='0' url='' flag='1' adverSign='0' multiMsgFlag='0'> <item><title>测试</title></item> <item layout='6' ><picture cover='https://w.wallhaven.cc/full/3z/wallhaven-3z32j3.jpg' /></item></msg>";
-    return new Message()
+    let message = new Message()
+    message
       .addText("新维修事件:\n")
       .addText("问题: ")
       .addText(event.user_description + "\n")
-      .addText("型号: ")
-      .addText(event.model + "\n")
+    if (event.model != null) {
+      message
+        .addText("型号: ")
+        .addText(event.model + "\n")
+    }
+
+    message
       .addText("www.vingilote.icu\n")
       .addAtAll();
-    // .addXml(xmlContent);
+    return message
+
   }
 
   async msgTest() {
