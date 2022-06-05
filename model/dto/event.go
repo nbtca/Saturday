69db3e0f3c2d109c7c009b185e36167b3314f6e4
Solsist <1443735959@qq.com>
2022-06-05 22:36:43 +0800

    event接口-成员 提交/修改提交/放弃 事件 Fixes #41

--- model/dto/event.go
+++ model/dto/event.go
@@ -3,3 +3,17 @@ package dto
 type EventID struct {
 	EventID int64 `uri:"EventId" json:"event_id" binding:"required"`
 }
+
+type CommitReq struct {
+	Content string `json:"content"`
+}
+
+type AlterCommitReq struct {
+	Content string `json:"content"`
+}
+
+type UpdateReq struct {
+	Phone   string `json:"phone" binding:"omitempty,len=11"`
+	QQ      string `json:"qq" binding:"omitempty,min=5,max=9"`
+	Problem string `json:"problem" db:"problem"`
+}
