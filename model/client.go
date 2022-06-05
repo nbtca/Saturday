c5533733cbfeef2157363a1be7da6dbb261b6c77
Jolan_yu <jolanyu@qq.com>
2022-06-05 23:12:02 +0800

    add wxlogin

--- model/client.go
+++ model/client.go
@@ -1,8 +1,8 @@
 package model
 
 type Client struct {
-	ClientId int64 `json:"client_id"`
-	Openid string `json:"openid"`
-	GmtCreate string `json:"gmt_create"`
-	GmtModified string `json:"gmt_modified"`
+	ClientId    int64  `json:"client_id" db:"client_id"`
+	OpenId      string `json:"openid"`
+	GmtCreate   string `json:"gmt_create" db:"gmt_create"`
+	GmtModified string `json:"gmt_modified" db:"gmt_modified"`
 }
