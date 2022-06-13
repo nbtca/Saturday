38a99b3c4b249ce1df63a0d6d1f12960b54e3b08
templari <q79142466@163.com>
2022-06-13 16:26:06 +0800

    save

--- model/dto/member.go
+++ model/dto/member.go
@@ -11,10 +11,10 @@ type CreateMemberReq struct {
 	Name     string `json:"name" binding:"required,min=2,max=4"`
 	Section  string `json:"section" binding:"required,section"`
 	Alias    string `json:"alias" binding:"omitempty,max=20"`
-	Avatar   string `json:"avatar" binding:"omitempty,url"`
+	Avatar   string `json:"avatar" binding:"omitempty,max=255"`
 	Profile  string `json:"profile" binding:"omitempty,max=1000"`
 	Phone    string `json:"phone" binding:"omitempty,len=11,numeric"`
-	QQ       string `json:"qq" binding:"omitempty,min=5,max=12,numeric"`
+	QQ       string `json:"qq" binding:"omitempty,min=5,max=20,numeric"`
 	Role     string `json:"role" binding:"required"`
 }
 
@@ -38,9 +38,9 @@ type UpdateMemberBasicReq struct {
 type UpdateMember struct {
 	MemberId string
 	Alias    string `json:"alias" binding:"omitempty,max=20"`
-	Avatar   string `json:"avatar" binding:"omitempty,url"`
+	Avatar   string `json:"avatar" binding:"omitempty,max=255"`
 	Profile  string `json:"profile" binding:"omitempty,max=1000"`
 	Phone    string `json:"phone" binding:"omitempty,len=11,numeric"`
-	QQ       string `json:"qq" binding:"omitempty,min=5,max=12,numeric"`
-	Password string `json:"password" binding:"omitempty"`
+	QQ       string `json:"qq" binding:"omitempty,min=5,max=20,numeric"`
+	Password string `json:"password" binding:"omitempty,max=20"`
 }
