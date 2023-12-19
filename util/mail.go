5486319188a1ca38c4853f47253eba39f46f0328
ClasWen <clas.wen@icloud.com>
2023-12-19 22:52:23 +0800

    fix mail

--- util/mail.go
+++ util/mail.go
@@ -11,12 +11,12 @@ var dialer *gomail.Dialer
 
 func InitDialer() {
 	host := os.Getenv("MAIL_HOST")
-	port, err := strconv.Atoi(os.Getenv("MAIL_HOST"))
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
 	if err != nil {
 		Logger.Error(err)
 	}
-	userName := os.Getenv("MAIL_HOST")
-	password := os.Getenv("MAIL_HOST")
+	userName := os.Getenv("MAIL_USERNAME")
+	password := os.Getenv("MAIL_PASSWORD")
 	dialer = gomail.NewDialer(host, port, userName, password)
 }
 
