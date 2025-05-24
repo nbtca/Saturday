2e3f7b046e4464ce2ec3dcda990573c93f2a0083
Clas Wen <clas.wen@icloud.com>
2025-05-24 16:55:16 +0800

    add sql

---
+++ migrations/000006_change_event_time_type.up.sql
@@ -0,0 +1,25 @@
+DROP VIEW public.event_view;
+
+ALTER TABLE "public"."event"
+  ALTER COLUMN "gmt_create" TYPE timestamp with time zone,
+  ALTER COLUMN "gmt_modified" TYPE timestamp with time zone;
+
+CREATE VIEW public.event_view AS
+ SELECT event.event_id,
+    event.client_id,
+    event.model,
+    event.phone,
+    event.qq,
+    event.contact_preference,
+    event.problem,
+    event.member_id,
+    event.closed_by,
+    event.gmt_create,
+    event.gmt_modified,
+    event.size,
+    COALESCE(event_status.status, ''::character varying) AS status,
+    event.github_issue_id,
+    event.github_issue_number
+   FROM ((public.event
+     LEFT JOIN public.event_event_status_relation ON ((event.event_id = event_event_status_relation.event_id)))
+     LEFT JOIN public.event_status ON ((event_event_status_relation.event_status_id = event_status.event_status_id)));
